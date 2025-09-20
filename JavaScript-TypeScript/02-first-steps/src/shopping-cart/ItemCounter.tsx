9b2a3ddd8d51bfd4d1e0f372118f11ee02a10c5f
FR4N1995 <fran7paco10@gmail.com>
2025-09-20 13:06:51 -0600

    se concluyo la seccion 4 del curso

--- JavaScript-TypeScript/02-first-steps/src/shopping-cart/ItemCounter.tsx
+++ JavaScript-TypeScript/02-first-steps/src/shopping-cart/ItemCounter.tsx
@@ -1,4 +1,7 @@
+import { useState } from "react";
 
+
+import './ItemCounter.css';
 interface Props {
     name: string,
     quantity?: number
@@ -6,18 +9,46 @@ interface Props {
 
 
 // donde esta name y quantity estamos desestructurando
-export const ItemCounter = ({name, quantity}:Props) => {
+export const ItemCounter = ({name, quantity = 1}:Props) => {
+
+  const [ count, setCount] = useState(quantity);
+
+  const handleAdd = () =>{  
+      setCount(count + 1);
+
+  }
+
+  const handleRest = () =>{
+    if(count === 1){
+      return;
+    }
+    setCount(count - 1)
+  }
+
+  const handleClick = () =>{
+    console.log(`click ${name}`);
+  }
+
   return (
-    <section style={{
-        display: 'flex',
-        alignItems: 'center',
-        gap: 10,
-        marginTop: 10
-    }}>
-        <span style={{width: 150}}>{name}</span>
-        <button>-1</button>
-        <span>{quantity}</span>
-        <button>+1</button>
+    <section 
+    className="item-row"
+    //  style={{    
+    //   display: 'flex',
+    //   alignItems: 'center',
+    //   gap: 10,
+    //   marginTop: 10
+    //  }}
+    >
+        <span className="item-text" style={{color: count === 1 ? 'red' : 'black'}}>{name}</span>
+        <button
+        onClick={handleRest}
+        >-1</button>
+        <span>{count}</span>
+        <button
+        onClick={() =>{
+          handleAdd();
+        }}
+        >+1</button>
     </section>
   )
 }
