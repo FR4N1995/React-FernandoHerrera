184ed8bdda51912fb31d6cf56434df13e220a6cc
FR4N1995 <fran7paco10@gmail.com>
2026-03-11 21:17:09 -0600

    add: casi acabamos seccion 13, maquetamos por componentes y ajustes a los componentes de terceros

--- src/heroes/pages/search/SearchPage.tsx
+++ src/heroes/pages/search/SearchPage.tsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import { CustomJumbotron } from '@/components/custom/CustomJumbotron'
+import { HeroStats } from '@/heroes/components/HeroStats'
+import { SearchControl } from './ui/SearchControl'
 
 export const SearchPage = () => {
   return (
-    <div>SearchPage</div>
+
+    <>
+      <CustomJumbotron title="Busqueda de SuperHeroes" description="Descubre, explora y administra super heroes y villanos" />
+      {/* Stats Dashboard */}
+      <HeroStats />
+
+      {/* filter searcch */}
+      <SearchControl />
+
+
+    </>
   )
 }
+
+
--- src/router/app.router.tsx
+++ src/router/app.router.tsx
@@ -5,9 +5,16 @@ import { HeroesLayout } from '@/heroes/layouts/HeroesLayout'
 import { HeroPage } from '@/heroes/pages/hero/HeroPage'
 import { HomePage } from '@/heroes/pages/home/HomePage'
 import { SearchPage } from '@/heroes/pages/search/SearchPage'
+import { lazy } from 'react'
 import {createBrowserRouter} from 'react-router'
 
+// esto es para que solo se cargue el modulo cuando se necesite;
+// const SearchPage = lazy(() => import('@/heroes/pages/search/SearchPage'))
+
 export const appRouter = createBrowserRouter([
+
+
+
 
     {
         path: '/',
